48e1ed5735b11e0f0c8b09dcbb4aeafa56b88f2a
ShubhangiD21 <shubadongare@gmail.com>
2020-08-28 18:39:46 +0530

    modified

---
+++ formJs.js
@@ -0,0 +1,16 @@
+function printData()
+{
+
+    var Fn=document.forms["stud"]["firstname"].value;
+    var Ln=document.forms["stud"]["lastname"].value;
+    var Des=document.forms["stud"]["deg"].value;
+    var Num=document.forms["stud"]["num"].value;
+    var Add=document.forms["stud"]["add"].value;
+    var City=document.forms["stud"]["city"].value;
+    var State=document.forms["stud"]["state"].value;
+    var Pin=document.forms["stud"]["pin"].value;
+    
+    document.write("Name is "+Fn);
+
+    
+}
